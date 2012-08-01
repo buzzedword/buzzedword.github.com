043f00f30e089b288725ec5ae99200a1bd42e73b
Daniel Garcia <danny@noisenewyork.com>
2012-08-01 15:53:12 -0400

    Set up grunt tasks.

---
+++ grunt.js
@@ -0,0 +1,98 @@
+/*global module:false*/
+module.exports = function(grunt) {
+
+  grunt.loadNpmTasks('grunt-recess');
+
+  // Recess helper function
+  var recessOptions = function(bool) {
+    bool = ((typeof bool === 'undefined')? false : bool);
+    var config = {
+      compile: true,
+      compress: bool,
+      noIDs: false,
+      noJSPrefix: false,
+      noOverqualifying: false,
+      noUnderscores: false,
+      noUniversalSelectors: false,
+      prefixWhitespace: false,
+      strictPropertyOrder: false,
+      zeroUnits: false
+    };
+
+    return config;
+  };
+
+  // Directory structure
+  var dirs = {};
+      dirs.assets = 'assets/';
+      dirs.css = dirs.assets + 'css/';
+      dirs.less = dirs.assets + 'less/';
+      dirs.img = dirs.assets + 'img/';
+      dirs.js = {};
+        dirs.js.base = dirs.assets + 'js/';
+        dirs.js.libs = dirs.js.base + 'libs/';
+        dirs.js.src = dirs.js.base + 'src/';
+      dirs.dist = {};
+        dirs.dist.base = 'dist/';
+        dirs.dist.js = dirs.dist.base + 'js/';
+        dirs.dist.css = dirs.dist.base + 'css/';
+        dirs.dist.img = dirs.dist.base + 'img/';
+
+
+  // Project configuration.
+  grunt.initConfig({
+    pkg: '<json:package.json>',
+    lint: {
+      files: ['grunt.js', dirs.js.src + '*.js']
+    },
+    watch: {
+      files: dirs.less + '*.less',
+      tasks: 'recess'
+    },
+    concat: {
+      libs: {
+        src : dirs.js.libs + '*.js',
+        dest: dirs.dist.js + 'libs.js'
+      }
+    },
+    min: {
+      libs: {
+        src : dirs.js.libs + '*.js',
+        dest: dirs.dist.js + 'libs.js'
+      }
+    },
+    recess: {
+      max: {
+        src: [dirs.less + 'base.less'],
+        dest: dirs.dist.css + 'style.css',
+        options: recessOptions(false)
+      },
+      min: {
+        src: [dirs.less + 'base.less'],
+        dest: dirs.dist.css + 'style.min.css',
+        options: recessOptions(true)
+      }
+    },
+    jshint: {
+      options: {
+        curly: true,
+        eqeqeq: true,
+        immed: true,
+        latedef: true,
+        newcap: true,
+        noarg: true,
+        sub: true,
+        undef: true,
+        boss: true,
+        eqnull: true,
+        browser: true
+      },
+      globals: {
+        jQuery: true
+      }
+    }
+  });
+
+  // Default task.
+  grunt.registerTask('default', 'recess');
+};
